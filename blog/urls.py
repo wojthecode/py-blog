c78574839bfc61cb5e257a36de4788f3b94d897b
Wojciech Witkowski <witus85@gmail.com>
2026-01-08 01:05:55 +0100

    fixed typo in login and urls

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from blog.views import IndexView, PostView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path("posts/<int:pk>", PostView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/", PostView.as_view(), name="post-detail"),
 ]
 
 app_name = "blog"
